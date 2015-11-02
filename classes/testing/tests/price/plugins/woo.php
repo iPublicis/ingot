1daa1196f1022ea71a42235d46184df600f8c6fb
Josh Pollock <jpollock412@gmail.com>
2015-11-02 15:42:47 -0500

    Add rough for the Woo price class

---
+++ classes/testing/tests/price/plugins/woo.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Handle testing for WooCommerce products
+ *
+ * @package   ingot
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2015 Josh Pollock
+ */
+
+namespace ingot\testing\tests\price\plugins;
+
+
+use ingot\testing\tests\price\price;
+
+class woo extends price {
+
+	/**
+	 * Add variable price hooks.
+	 *
+	 * @since 0.1.2
+	 *
+	 * @access protected
+	 */
+	protected function variable_price_hooks() {
+
+	}
+
+	/**
+	 * Add non-variable price hook.
+	 *
+	 * @since 0.1.2
+	 *
+	 * @access protected
+	 */
+	protected function non_variable_price_hooks() {
+
+	}
+
+	/**
+	 * Use in subclass to set the variable and prices properties
+	 *
+	 * @since 0.1.2
+	 *
+	 * @access protected
+	 */
+	protected function set_price() {
+		$this->variable = true; //?
+		$this->prices = true; //?
+	}
+
+}
